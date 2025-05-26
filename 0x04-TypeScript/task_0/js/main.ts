6b73faf09e6892af359181a91c32a5b5713d4acb
sbusisoShaper <sbusisomdlalose@thedigitalacademy.co.za>
2025-05-26 22:20:57 +0200

    Task1

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,21 @@
+interface Student {
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string
+}
+
+const student1: Student = {
+    firstName: "Sbusiso",
+    lastName: "Zulu",
+    age: 25,
+    location: "Johannesburg"
+  };
+  
+const student2: Student = {
+    firstName: "Zanele",
+    lastName: "Nkosi",
+    age: 23,
+    location: "Cape Town"
+  };
+  
