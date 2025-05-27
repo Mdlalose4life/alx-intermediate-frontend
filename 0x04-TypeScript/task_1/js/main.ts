b22daa9e324220c0d7b97ea6feb1df7ca2700bb1
sbusisoShaper <sbusisomdlalose@thedigitalacademy.co.za>
2025-05-27 20:39:19 +0200

    Task_1

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -3,5 +3,71 @@ interface Teacher {
     readonly lastName: string;
     fullTimeEmployee: boolean;
     yearsOfExperience ?: number;
-    location: string
+    location: string;
+    [key: string]: any
+}
+
+const teacher3: Teacher = {
+    firstName: 'John',
+    fullTimeEmployee: false,
+    lastName: 'Doe',
+    location: 'London',
+    contract: false,
+  }
+
+  console.log(teacher3);
+
+
+
+  interface Directors extends Teacher{
+    numberOfReports: number
+  }
+
+const director1: Directors = {
+    firstName: 'John',
+    fullTimeEmployee: false,
+    lastName: 'Doe',
+    location: 'London',
+    contract: false,
+    numberOfReports: 21
+  }
+
+  console.log(director1);
+
+interface printTeacher{
+      (firstName: string, lastName: string): string;
+}
+
+function printTeacher(firstName:string, lastName:string) {
+    return `${firstName.charAt(0)}. ${lastName}`
+}
+
+console.log((printTeacher("Sbusiso", "Mdlalose")));
+
+// Define an interface for the constructor
+export interface Studentconstructor {
+    firstName: string;
+    lastName: string;
+}
+export interface StudentClassInterface {
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+export class StudentClass implements StudentClassInterface {
+    firstName: string;
+    lastName: string;
+
+    constructor({firstName, lastName}: Studentconstructor){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return 'Currently working'
+    }
+
+    displayName(): string {
+        return this.firstName
+    }
 }
--- 0x04-TypeScript/task_1/webpack.config.js
+++ 0x04-TypeScript/task_1/webpack.config.js
@@ -1,37 +1,39 @@
-const path = require("path");
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = {
-  entry: "./js/main.ts",
-  devtool: "inline-source-map",
+  entry: './js/main.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development',
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
-        loader: 'ts-loader',
-        options: {
-          transpileOnly: true
-        }
-      }
-    ]
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"]
+    extensions: ['.ts', '.js'],
   },
   devServer: {
-    contentBase: "./dist"
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+      watch: true,
+    },
+    open: true,
+    hot: true,
+    port: 8080,
   },
   plugins: [
-    new ForkTsCheckerWebpackPlugin(),
-    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: "Development"
-    })
+      template: './index.html',
+    }),
+    new CleanWebpackPlugin(),
   ],
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist")
-  }
 };
